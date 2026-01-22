4db50efc9a6eb81d40841f38dfeb3547d6840d1a
duw4ng <alexavalos330@gmail.com>
2026-01-22 04:05:18 -0300

    Implement cd command functionality in cd.c
    Hecho por: Alexandre Avalos
    Implementacion del comando cd.
    * Política: permanecer en el directorio actual en caso de no haber argumentos.

---
+++ src/cd.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#define MAX_PATH 1024
+
+int ejecCd(char *directorio){
+    char cwd_actual[MAX_PATH];
+    char cwd_nuevo[MAX_PATH];
+    if (!getcwd(cwd_actual, sizeof(cwd_actual))){
+        perror("cd");
+        return 1;
+    }
+    
+    if (!directorio){
+        printf("ERROR. Se necesita de un directorio al cual ir.\n");
+        return 1;
+    }
+    
+    if (getcwd(cwd_nuevo, sizeof(cwd_nuevo)) == NULL) {
+        perror("cd");
+        return 1;
+    }
+    
+    if (chdir(directorio) == -1) {
+        perror("cd");
+        return 1;
+    }
+    
+    setenv("OLDPWD", cwd_actual, 1);
+    setenv("PWD", cwd_nuevo, 1);
+
+    return 0;
+}
